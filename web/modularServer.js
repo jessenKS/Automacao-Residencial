80126d80e30cbf4fba955fb898d3fd8e1bb1260a
mateusfrizon <56055202+mateusfrizon@users.noreply.github.com>
2020-06-16 12:46:10 -0300

    Update modularServer.js

--- web/modularServer.js
+++ web/modularServer.js
@@ -65,7 +65,7 @@ socket.on('connection', function(client) {
       
       mensagem = data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7] + data[8] + data[9] + data[10] + data[11] + data[12];
       lrc = LRC(mensagem)
-      mensagem = data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7] + data[8] + data[9] + data[10] + data[11] + data[11] + lrc;
+      mensagem = data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7] + data[8] + data[9] + data[10] + data[11] + data[12] + lrc;
       console.log("Mensagem:" + mensagem);
       sPort.write(mensagem);
     });
